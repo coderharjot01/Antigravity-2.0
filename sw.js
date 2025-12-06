33c0af658a13941b1a2ba2c69feaf833728bef62
HARJOTASINGH0001 <harjotsinghas01@gmail.com>
2025-12-06 18:00:20 +0530

    Convert website to PWA (add manifest and service worker)

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'hs21-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/services.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Strategy: Cache First, then Network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return cached response if found
+                if (response) {
+                    return response;
+                }
+                // Otherwise fetch from network
+                return fetch(event.request);
+            })
+    );
+});
